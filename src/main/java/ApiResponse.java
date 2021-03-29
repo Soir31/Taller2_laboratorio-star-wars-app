73c3a9abac0e83f8edb99f53519b72c80200de96
Daniel <danov54@estudiantec.cr>
2021-03-29 11:14:29 -0600

    Added new stuff

--- src/main/java/ApiResponse.java
+++ src/main/java/ApiResponse.java
@@ -1,25 +1,25 @@
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
+//import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ApiResponse<T> {
-    private long count;
-    private List<T> results;
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class ApiResponse<T> {
+//    private long count;
+//    private List<T> results;
 
-    public long getCount() {
-        return count;
-    }
+//    public long getCount() {
+//        return count;
+//    }
 
-    public void setCount(long count) {
-        this.count = count;
-    }
+//    public void setCount(long count) {
+//        this.count = count;
+//    }
 
-    public List<T> getResults() {
-        return results;
-    }
+//    public List<T> getResults() {
+//        return results;
+//    }
 
-    public void setResults(List<T> results) {
-        this.results = results;
-    }
-}
+//    public void setResults(List<T> results) {
+//        this.results = results;
+//    }
+//}
--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,23 +1,23 @@
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Character {
-    private String name;
-    private float height;
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class Character {
+//    private String name;
+//    private float height;
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
-    public float getHeight() {
-        return height;
-    }
+//    public float getHeight() {
+//        return height;
+//    }
 
-    public void setHeight(float height) {
-        this.height = height;
-    }
-}
+//    public void setHeight(float height) {
+//        this.height = height;
+//    }
+//}
